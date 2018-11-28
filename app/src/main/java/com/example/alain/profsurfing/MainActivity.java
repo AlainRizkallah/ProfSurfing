ca8f6cb229fef42099aa9aa328376eff0cea0acb
AlainRizkallah <alain.rizkallah@isep.fr>
2018-11-28 15:17:19 +0100

    Login draft

--- app/src/main/java/com/example/alain/profsurfing/MainActivity.java
+++ app/src/main/java/com/example/alain/profsurfing/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.alain.profsurfing;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
 public class MainActivity extends Activity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 }
