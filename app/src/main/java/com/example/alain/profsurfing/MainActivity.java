eee5fa1498ed51c9218152ad3d511be97db24968
AlainRizkallah <alain.rizkallah@isep.fr>
2018-12-05 16:47:53 +0100

    Check if user is signed in (non-null) and update UI accordingly.
    dans tous les cas, ça redirige vers LoginActivity
    => voir coment renvoyer sur le bon profil / calendrier

--- app/src/main/java/com/example/alain/profsurfing/MainActivity.java
+++ app/src/main/java/com/example/alain/profsurfing/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.alain.profsurfing;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -14,11 +22,13 @@ public class MainActivity extends Activity {
 
     private FirebaseAuth mAuth;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        onStart();
 
     }
 
@@ -26,9 +36,25 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
-
-    public void createAccount(View view) {
-        Intent intent = new Intent (this,CreateAccountActivity.class );
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser==null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            }
+        else{
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
 }
